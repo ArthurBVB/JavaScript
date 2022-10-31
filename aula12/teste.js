58784499a31f92db86e8afb7b30b71a91453382d
Arthur Bueno Vilas Boas <arthurbvb@hotmail.com>
2022-10-31 17:02:53 -0300

    novas aulaas

---
+++ aula12/teste.js
@@ -0,0 +1,17 @@
+//console.log('Tudo bem?')
+var c = 1
+do {
+    console.log(`passo ${c}`)
+    c++
+}while(c <= 6)
+
+
+
+
+
+/*var c = 1
+while(c <= 6) {
+    console.log(`passo ${c}`)
+    c++
+}
+*/
